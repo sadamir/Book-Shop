b23a7a4418d072d2b3d4cf614577ad4d7399ae3f
sadamir <a.saduanov19@gmail.com>
2022-05-24 03:56:21 +0500

    feat: create order form

--- app.js
+++ app.js
@@ -74,14 +74,14 @@ divProductTitle.appendChild(title);
 
 
 let items_array = [
-    { "title": "Summer Bird Blue", "id": 1, image: "./images/book-1.jpeg", "sign": "$", price: 12.99 },
-    { "title": "What You Think", "id": 2, image: "./images/book-2.jpeg", "sign": "$", price: 32.99 },
-    { "title": "Boring Girls", "id": 3, image: "./images/book-3.jpeg", "sign": "$", price: 43.99 },
-    { "title": "Unleash the Dragon", "id": 4, image: "./images/book-4.jpeg", "sign": "$", price: 54.99 },
-    { "title": "A Million to One", "id": 5, image: "./images/book-5.jpeg", "sign": "$", price: 65.99 },
-    { "title": "Edit This", "id": 6, image: "./images/book-6.jpeg", "sign": "$", price: 14.98 },
-    { "title": "Design Formula", "id": 7, image: "./images/book-7.jpeg", "sign": "$", price: 32.99 },
-    { "title": "NoName Book", "id": 8, image: "./images/book-8.jpeg",  "sign": "$", price: 52.99 }
+    { "title": "Summer Bird Blue", "id": 1, image: "./images/book-1.jpeg", "sign": "$", price: 12.99, "author": "Akemi Dawn", "description": "Effective JavaScript is organized around 68 proven approaches for writing better JavaScript, backed by concrete examples. You’ll learn how to choose the right programming style for each project, manage unanticipated problems, and work more successfully with every facet of JavaScript programming from data"},
+    { "title": "What You Think", "id": 2, image: "./images/book-2.jpeg", "sign": "$", price: 32.99, "author": "Michael McAffe", "description": "Effective JavaScript is organized around 68 proven approaches for writing better JavaScript, backed by concrete examples. You’ll learn how to choose the right programming style for each project, manage unanticipated problems, and work more successfully with every facet of JavaScript programming from data"},
+    { "title": "Boring Girls", "id": 3, image: "./images/book-3.jpeg", "sign": "$", price: 43.99, "author": "Lee Cooper", "description": "Effective JavaScript is organized around 68 proven approaches for writing better JavaScript, backed by concrete examples. You’ll learn how to choose the right programming style for each project, manage unanticipated problems, and work more successfully with every facet of JavaScript programming from data"},
+    { "title": "Unleash the Dragon", "id": 4, image: "./images/book-4.jpeg", "sign": "$", price: 54.99, "author": "Jackie Chan", "description": "Effective JavaScript is organized around 68 proven approaches for writing better JavaScript, backed by concrete examples. You’ll learn how to choose the right programming style for each project, manage unanticipated problems, and work more successfully with every facet of JavaScript programming from data"},
+    { "title": "A Million to One", "id": 5, image: "./images/book-5.jpeg", "sign": "$", price: 65.99, "author": "Luci Lu", "description": "Effective JavaScript is organized around 68 proven approaches for writing better JavaScript, backed by concrete examples. You’ll learn how to choose the right programming style for each project, manage unanticipated problems, and work more successfully with every facet of JavaScript programming from data"},
+    { "title": "Edit This", "id": 6, image: "./images/book-6.jpeg", "sign": "$", price: 14.98, "author": "Van Damme", "description": "Effective JavaScript is organized around 68 proven approaches for writing better JavaScript, backed by concrete examples. You’ll learn how to choose the right programming style for each project, manage unanticipated problems, and work more successfully with every facet of JavaScript programming from data"},
+    { "title": "Design Formula", "id": 7, image: "./images/book-7.jpeg", "sign": "$", price: 32.99, "author": "Vlad Zelen","description": "Effective JavaScript is organized around 68 proven approaches for writing better JavaScript, backed by concrete examples. You’ll learn how to choose the right programming style for each project, manage unanticipated problems, and work more successfully with every facet of JavaScript programming from data"},
+    { "title": "NoName Book", "id": 8, image: "./images/book-8.jpeg",  "sign": "$", price: 52.99, "author": "Tobi McGuer","description": "Effective JavaScript is organized around 68 proven approaches for writing better JavaScript, backed by concrete examples. You’ll learn how to choose the right programming style for each project, manage unanticipated problems, and work more successfully with every facet of JavaScript programming from data"}
 ];
 
 //single product
@@ -111,6 +111,42 @@ productsDOM.setAttribute('class', 'products-center');
 productsDOM.setAttribute('id', 'container');
 sectionProducts.appendChild(productsDOM);
 
+const productsPopup = document.createElement('div');
+productsPopup.setAttribute('class', 'products-popup');
+productsPopup.setAttribute('id', 'p-popup');
+sectionProducts.appendChild(productsPopup);
+
+// const popupContent = document.createElement('div');
+// popupContent.setAttribute('class', 'popup-content');
+// popupContent.setAttribute('id', 'popup-content');
+// productsPopup.appendChild(popupContent);
+
+// function popupArticle(items, container) {
+//     let popup_container = getDiv(container);
+//     popup_container.innerHTML = '';
+//
+//     for (let i = 0; i < items.length; i++) {
+//         let item = items[i];
+//
+//         let item_node = createNode("div");
+//         item_node.setAttribute("id", item.id);
+//         item_node.setAttribute("class", 'popup-content');
+//
+//         let item_img = createNode("img");
+//         item_img.setAttribute('src', item.image);
+//         item_img.setAttribute('alt', 'book-img');
+//         item_img.setAttribute('class', 'popup-img');
+//
+//         if (item.price > 0) {
+//             item_node.innerHTML = `<h3>${item.title}</h3><h3>${item.author}</h3>${item.description}`;
+//             appendNode(popup_container, item_node);
+//             appendNode(item_node, item_img);
+//         }
+//     }
+// }
+// popupArticle(items_array, "container");
+
+
 const articleProducts = document.createElement('article');
 articleProducts.setAttribute('class', 'product');
 articleProducts.setAttribute('id', 'items');
@@ -128,13 +164,12 @@ function displayItems(items, container) {
         item_node.setAttribute("class", 'img-container');
 
         let item_img = createNode("img");
-        //item_img.setAttribute('src', "images/book-" + i + ".jpeg");
         item_img.setAttribute('src', item.image);
         item_img.setAttribute('alt', 'product');
         item_img.setAttribute('class', 'product-img');
 
         if (item.price > 0) {
-            item_node.innerHTML = `<button class="bag-btn" data-id=${i}>add to bag</button><h3>${item.title}</h3>${' '}<h4>${item.sign}
+            item_node.innerHTML = `<button class="showmore-btn" data-id=${i}>show more</button><button class="bag-btn" data-id=${i}>add to bag</button><h2>${item.title}</h2><h3>${item.author}</h3>${' '}<h4>${item.sign}
             <span id="badge">${item.price}</span></h4>`;
             appendNode(items_container, item_node);
             appendNode(item_node, item_img);
@@ -213,15 +248,27 @@ buttons.forEach(button => {
       });
 
       function cartLogic() {
-
+        clearCartBtn.addEventListener('click', () => {
+          this.clearCart();
+        })
       }
 
+        // function clearCart(){
+        //   let elem =
+        //   cartContent.
+        // }
 
 
     })
   })
 
+//popup
 
+
+
+
+
+//end of popup
 
 
 //end of products
@@ -253,50 +300,6 @@ const cartContent = document.createElement('div');
 cartContent.setAttribute('class', 'cart-content');
 cartDOM.appendChild(cartContent);
 
-//cart item
-// const divCartItemList = document.createElement('div');
-// divCartItemList.setAttribute('class', 'cart-item');
-// cartContent.appendChild(divCartItemList);
-//
-// const imgCartList = document.createElement('img');
-// imgCartList.setAttribute('src', 'images/book-1.jpeg');
-// imgCartList.setAttribute('alt', 'product');
-// imgCartList.setAttribute('class', 'product-img');
-// divCartItemList.appendChild(imgCartList);
-//
-// const divCartItemInfo = document.createElement('div');
-// divCartItemList.appendChild(divCartItemInfo);
-//
-// const cartItemName = document.createElement('h4');
-// cartItemName.textContent = 'Summer Bird Blue';
-// divCartItemInfo.appendChild(cartItemName);
-//
-// const cartItemPrice = document.createElement('h5');
-// cartItemPrice.textContent = '$123';
-// divCartItemInfo.appendChild(cartItemPrice);
-//
-// const cartItemRemove = document.createElement('span');
-// cartItemRemove.setAttribute('class', 'remove-item');
-// cartItemRemove.textContent = 'remove';
-// divCartItemInfo.appendChild(cartItemRemove);
-//
-// const divCartChevrons = document.createElement('div');
-// divCartItemList.appendChild(divCartChevrons);
-//
-// const iChevronUp = document.createElement('i');
-// iChevronUp.setAttribute('class', 'fas fa-chevron-up');
-// divCartChevrons.appendChild(iChevronUp);
-//
-// const pItemAmount = document.createElement('p');
-// pItemAmount.setAttribute('class', 'item-amount');
-// pItemAmount.textContent = 1;
-// divCartChevrons.appendChild(pItemAmount);
-//
-// const iChevronDown = document.createElement('i');
-// iChevronDown.setAttribute('class', 'fas fa-chevron-down');
-// divCartChevrons.appendChild(iChevronDown);
-//end of cart item
-
 const divCartFooter = document.createElement('div');
 divCartFooter.setAttribute('class', 'cart-footer');
 cartDOM.appendChild(divCartFooter);
@@ -312,48 +315,17 @@ totalCartInfoText.appendChild(cartTotal);
 
 const clearCartBtn = document.createElement('button');
 clearCartBtn.setAttribute('class', 'clear-cart banner-btn');
-clearCartBtn.textContent = 'clear cart';
+clearCartBtn.textContent = 'confirm order';
 divCartFooter.appendChild(clearCartBtn);
 
 //end of Cart
 
+clearCartBtn.onclick = function () {
+  location.href = "order-form.html";
+}
 
 //logic
 
-cartBtn
-closeCartBtn
-clearCartBtn
-cartDOM
-cartOverlay
-cartItems
-cartTotal
-cartContent
-//productsDOM
+class UI {
 
-
-
-
-// //cart
-// let cart = [];
-//
-// //getting the productsDOM
-//
-// //display productsDOM
-// class UI{
-//
-// }
-//
-// //local storage
-// class Storage{
-//
-// }
-
-
-// document.addEventListener("DOMContentLoaded",()=>{
-//   const ui = new UI();
-//   const products = new Products();
-//
-//   // get all products
-//   products.getProducts().then(data => console.log(data));
-//
-// })
+}
---
+++ order.js
@@ -0,0 +1,7 @@
+const name = document.getElementById('name');
+const forms = document.getElementById('forms');
+
+forms.addEventListener('submit', (e) => {
+  let messages = [];
+  if (name.value === '' || name.value === null || name.value !== isString)
+})
